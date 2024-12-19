2cb0b1e5a8a65ad6d0e798cf319a51c52d54a4cd
XcentriXTV <daniel.cope1992@googlemail.com>
2024-12-19 18:15:16 +0000

    Set up basic game loop with black window

---
+++ main.py
@@ -0,0 +1,26 @@
+# this allows us to use code from the open source pygame library throughout this file
+import pygame
+from constants import *
+
+def main():
+    pygame.init()  
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    print("Starting asteroids!")
+    
+    while True: # Add the game loop
+        # Handle events
+            
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        
+        # Fill screen with black
+        screen.fill("black")
+            
+        # update display
+        pygame.display.flip()
+       
+if __name__ == "__main__":
+    main()
